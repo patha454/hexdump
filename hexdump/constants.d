c1044d79b59dbc0b5eafbc9f7b6584e9b98c57e9
patha454 <patha454@student.otago.ac.nz>
2017-05-04 18:10:58 +1200

    Removed magic numbers

---
+++ hexdump/constants.d
@@ -0,0 +1,14 @@
+/**
+* Constants for the hexdump application
+*
+* Author: H Paterson, University of Otago, 2017
+*/
+
+module hexdump.constants;
+
+
+///Size of a megabyte;
+package immutable uint MB = 1024;
+
+///Word length, in bits, to dump;
+package immutable uint WORD_LENGTH = 8;
